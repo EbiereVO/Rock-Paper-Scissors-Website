125b746cdf27ff2b8e71a039543b01ff48e74426
ebiere <e.v.orubide@tilburguniversity.edu>
2020-10-26 20:07:22 +0100

    updates

--- rock-paper-scissors-master/script.js
+++ rock-paper-scissors-master/script.js
@@ -1,10 +1,12 @@
-const buttons = document.querySelectorAll(".btn-circle");
-const choices = ["paper", "rock", "scissors"];
+const buttons = document.querySelectorAll(".pick");
 const scoreEl = document.getElementById("score");
+const main = document.getElementById("main");
+const selection = document.getElementById("selection");
+const reset = document.getElementById("reset");
+
+const choices = ["paper", "rock", "scissors"];
+
 let score = 0;
-
-// console.log(pickRandomChoice());
-
 let userChoice = undefined;
 
 buttons.forEach((button) => {
@@ -12,9 +14,12 @@ buttons.forEach((button) => {
     userChoice = button.getAttribute("data-choice");
 
     checkWinner();
-
-    // console.log(userChoice);
   });
+});
+
+reset.addEventListener("click", () => {
+  main.style.display = "flex";
+  selection.style.display = "none";
 });
 
 function checkWinner() {
@@ -32,6 +37,9 @@ function checkWinner() {
     // user lost
     updateScore(-1);
   }
+  //   show the selection | hide the main
+  main.style.display = "none";
+  selection.style.display = "flex";
 }
 
 function updateScore(value) {
